9189f85b378615aac291edc444c6add85e754646
asmbatati <asmalbatati@hotmail.com>
2025-04-29 12:16:45 +0300

    Modified launch and rviz configs

--- launch/dem.launch.py
+++ launch/dem.launch.py
@@ -2,15 +2,35 @@
 
 import os
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, OpaqueFunction
 from launch_ros.actions import Node
 from ament_index_python import get_package_share_directory
 from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 
-def generate_launch_description():
-    ld = LaunchDescription()
+def launch_setup(context, *args, **kwargs):
+    world_type = LaunchConfiguration('world_type').perform(context)
+    localization_model = LaunchConfiguration('localization_model').perform(context)
+
+    # Set world and position based on world_type
+    if world_type == 'taif_world':
+        xpos, ypos, zpos = '-50.0', '100.0', '2000.0'
+    elif world_type == 'dem_world':
+        xpos, ypos, zpos = '0.0', '200.0', '900.0'
+    elif world_type == 'tugbot_depot':
+        xpos, ypos, zpos = '0.0', '0.0', '0.1'
+    else:
+        xpos, ypos, zpos = '0.0', '0.0', '1.0'
+    w_name = world_type
+
+    # RViz config selection
+    package_share_directory = get_package_share_directory('gps_denied_navigation_sim')
+    if localization_model == 'ov':
+        rviz_file_name = 'dem_ov.rviz'
+    else:
+        rviz_file_name = 'dem_mins.rviz'
+    rviz_file_path = os.path.join(package_share_directory, rviz_file_name)
 
     # gz node
     m_name = 'x500_mono_cam_3d_lidar'
@@ -19,29 +39,7 @@ def generate_launch_description():
     # for original dem use dem_world
     # for Taif DEM use taif_world
     # For empty world use default
-    w_name='taif_world'
     # w_name='tugbot_depot'
-    world_name = {'gz_world': w_name}
-    autostart_id = {'px4_autostart_id': '4022'}
-    instance_id = {'instance_id': f'{m_id}'}
-    # for taif DEM use
-    # xpos = {'xpos': '135.0'}
-    # ypos = {'ypos': '100.0'}
-    # zpos = {'zpos': '2000.0'}
-
-    # For original DEM use
-    # xpos = {'xpos': '0.0'}
-    # ypos = {'ypos': '200.0'}
-    # zpos = {'zpos': '900.0'}
-    
-    # for taif DEM use
-    xpos = {'xpos': '-50.0'}
-    ypos = {'ypos': '100.0'}
-    zpos = {'zpos': '2000.0'}
-    # for tugbot_depot DEM use
-    # xpos = {'xpos': '0.0'}
-    # ypos = {'ypos': '0.0'}
-    # zpos = {'zpos': '0.1'}
     headless= {'headless' : '0'}
 
     # Namespace
@@ -59,18 +57,17 @@ def generate_launch_description():
             'gz_ns': ns,
             'headless': headless['headless'],
             'gz_model_name': model_name['gz_model_name'],
-            'gz_world': world_name['gz_world'],
-            'px4_autostart_id': autostart_id['px4_autostart_id'],
-            'instance_id': instance_id['instance_id'],
-            'xpos': xpos['xpos'],
-            'ypos': ypos['ypos'],
-            'zpos': zpos['zpos']
+            'gz_world': w_name,
+            'px4_autostart_id': '4022',
+            'instance_id': f'{m_id}',
+            'xpos': xpos,
+            'ypos': ypos,
+            'zpos': zpos
         }.items()
     )
 
     # MAVROS
     file_name = 'target_px4_pluginlists.yaml'
-    package_share_directory = get_package_share_directory('gps_denied_navigation_sim')
     plugins_file_path = os.path.join(package_share_directory, file_name)
     file_name = 'target_px4_config.yaml'
     config_file_path = os.path.join(package_share_directory, file_name)
@@ -100,7 +97,7 @@ def generate_launch_description():
         package='tf2_ros',
         name='map2px4_'+ns+'_tf_node',
         executable='static_transform_publisher',
-        arguments=[str(xpos['xpos']), str(ypos['ypos']), '0', '0', '0', '0', map_frame, ns+'/'+odom_frame],
+        arguments=[xpos, ypos, '0', '0', '0', '0', map_frame, ns+'/'+odom_frame],
     )
 
     # Static TF target/base_link to lidar link
@@ -197,9 +194,6 @@ def generate_launch_description():
         output='screen',
          )
 
-    rviz_file_name = 'dem.rviz'
-    package_share_directory = get_package_share_directory('gps_denied_navigation_sim')
-    rviz_file_path = os.path.join(package_share_directory, rviz_file_name)
     rviz_node = Node(
             package='rviz2',
             executable='rviz2',
@@ -208,13 +202,26 @@ def generate_launch_description():
             arguments=['-d', rviz_file_path],
         )
 
+    # Add all your actions to a list and return
+    return [
+        gz_launch,
+        map2pose_tf_node,
+        base2lidar_tf_node,
+        mavros_launch,
+        # random_trajectories_node,  # Uncomment if you want this node
+        gimbal_node,
+        ros_gz_bridge,
+        rviz_node,
+    ]
 
-    ld.add_action(gz_launch)
-    ld.add_action(map2pose_tf_node)
-    ld.add_action(base2lidar_tf_node)
-    ld.add_action(mavros_launch)
-    # ld.add_action(random_trajectories_node)
-    ld.add_action(gimbal_node)
-    ld.add_action(ros_gz_bridge)
-    ld.add_action(rviz_node)
+def generate_launch_description():
+    ld = LaunchDescription()
+    world_type_arg = DeclareLaunchArgument(
+        'world_type', default_value='taif_world', description='World type: taif_world, dem_world, tugbot_depot')
+    localization_model_arg = DeclareLaunchArgument(
+        'localization_model', default_value='mins', description='Localization model: mins or ov')
+
+    ld.add_action(world_type_arg)
+    ld.add_action(localization_model_arg)
+    ld.add_action(OpaqueFunction(function=launch_setup))
     return ld
